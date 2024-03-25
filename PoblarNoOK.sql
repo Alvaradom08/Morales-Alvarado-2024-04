c269575a5d5cda4102ab0f7531bf65ce7c8d7336
CamMorales01 <128505448+CamMorales01@users.noreply.github.com>
2024-03-24 22:49:31 -0500

    DESARROLLO

---
+++ PoblarNoOK.sql
@@ -0,0 +1,4 @@
+--PoblarNoOK
+
+--no deja agregar los datos debido a que el tipo de dato ingresado en IDMONEDA es de tipo caracter y no numerico.
+INSERT INTO MONEDA(IDMONEDA,CANTIDAD,TIPODEMONEDA) VALUES(0001,100.00,'ValorantPoint');
---
+++ Tablas.sql
@@ -0,0 +1,63 @@
+--Tablas
+
+CREATE TABLE Inventario(
+    idObjeto CHAR(5) NOT NULL,
+    tipo VARCHAR(8) NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    color INT NOT NULL
+);
+
+CREATE TABLE InventarioTienda(
+    idObjeto CHAR(5) NOT NULL,
+    precioV NUMBER(5,2)NOT NULL,
+    descripcion VARCHAR(100) NOT NULL,
+    precioR NUMBER(5,2) NOT NULL
+);
+
+CREATE TABLE InventarioUsuario (
+    idObjeto CHAR(5) NOT NULL,
+    fechadecompra DATE NULL, 
+    fechadeintercambio DATE NULL
+);
+
+CREATE TABLE Linea (
+    objeto VARCHAR(20) NOT NULL,
+    precioVenta NUMBER(5,2) NOT NULL
+);
+
+CREATE TABLE Compra (
+    numero CHAR(5) NOT NULL,
+    fecha DATE NOT NULL,
+    total NUMBER(5,2) NOT NULL
+);
+
+CREATE TABLE Jugador (
+    idJugador CHAR(5) NOT NULL,
+    nickName VARCHAR(20) NOT NULL,
+    correo VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Moneda (
+    idMoneda CHAR(5) NOT NULL,
+    cantidad NUMBER(5,2) NOT NULL,
+    tipodemoneda CHAR(13) NOT NULL
+);
+
+CREATE TABLE Intercambio (
+    numero CHAR(5) NOT NULL,
+    objeto CHAR(5) NOT NULL,
+    valor NUMBER(5,2) NOT NULL,
+    fecha DATE NOT NULL
+);
+
+CREATE TABLE Articulo (
+    idTienda CHAR(5) NOT NULL,
+    idLinea CHAR(5) NOT NULL,
+    precioVenta NUMBER(5,2) NOT NULL
+);
+
+CREATE TABLE Objeto (
+    numero CHAR(5) NOT NULL,
+    idObjeto CHAR(5) NOT NULL,
+    nombre VARCHAR(50) NOT NULL
+);
