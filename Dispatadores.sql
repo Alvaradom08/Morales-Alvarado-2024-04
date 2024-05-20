0bec008484ec6adf7bbac84000b1df925ad9534b
Alvaradom08 <112224703+Alvaradom08@users.noreply.github.com>
2024-05-19 23:55:47 -0500

    Add files via upload

--- Dispatadores.sql
+++ Dispatadores.sql
@@ -6,9 +6,9 @@ CREATE SEQUENCE seq_cp START WITH 1 INCREMENT BY 1 NOCACHE;
 CREATE SEQUENCE seq_tr START WITH 1 INCREMENT BY 1 NOCACHE;
 CREATE SEQUENCE seq_in START WITH 1 INCREMENT BY 1 NOCACHE;
 ------------------------------------------------------------
---TRIGGER PARA GENERAR ID DE INVENTARIO
-CREATE OR REPLACE TRIGGER trg_autogenerar_id
-BEFORE INSERT ON Inventario
+-- TRIGGER PARA GENERAR ID DE INVENTARIOS
+CREATE OR REPLACE TRIGGER trg_autogenerar_idInventarios
+BEFORE INSERT ON Inventarios
 FOR EACH ROW
 DECLARE
     v_prefix CHAR(1);
@@ -26,106 +26,72 @@ BEGIN
         v_prefix := 'S';
         SELECT seq_skin.NEXTVAL INTO v_nextval FROM dual;
     ELSE
-        RAISE_APPLICATION_ERROR(-20001, 'Tipo de objeto no válido');
+        RAISE_APPLICATION_ERROR(-20001, 'Tipo de objeto no v�lido');
     END IF;
 
-    -- Formatea el valor numérico de la secuencia
+    -- Formatea el valor num�rico de la secuencia
     v_sequence := TO_CHAR(v_nextval, 'FM0000');
 
     -- Asigna el valor generado a la columna id
-    :new.id := v_prefix || v_sequence;
+    :NEW.id := v_prefix || v_sequence;
 END;
-------------------------------------------------------------
---TRIGGER PARA GENERAR ID DE JUGADOR
-CREATE SEQUENCE seq_jugador START WITH 1 INCREMENT BY 1 NOCACHE;
+/
 
+-- TRIGGER PARA GENERAR ID DE JUGADORES
 CREATE OR REPLACE TRIGGER trg_autogenerar_idJugador
-BEFORE INSERT ON Jugador
+BEFORE INSERT ON Jugadores
 FOR EACH ROW
 BEGIN
     :NEW.idJugador := 'U' || LPAD(seq_jugador.NEXTVAL, 4, '0');
 END;
-------------------------------------------------------------
---TRIGGER PARA GENERAR ID DE TRANSACCIONES
-CREATE OR REPLACE TRIGGER trg_autogenerar_id_transacciones
-BEFORE INSERT ON TRANSACCIONES
+/
+
+-- TRIGGER PARA GENERAR ID DE TRANSACCIONES
+CREATE OR REPLACE TRIGGER trg_autogenerar_idTransacciones
+BEFORE INSERT ON TRASNSACCIONES
 FOR EACH ROW
-DECLARE
-    v_sequence CHAR(3);
-    v_nextval NUMBER;
 BEGIN
-    SELECT seq_tr.NEXTVAL INTO v_nextval FROM dual;
-
-    -- Formatear el valor numérico de la secuencia
-    v_sequence := LPAD(v_nextval, 3, '0');
-
-    -- Asignar el valor generado a la columna idTransaccione
-    :NEW.idTransaccione := 'TR' || v_sequence;
+    :NEW.idTrasnsaccion := 'TR' || LPAD(seq_tr.NEXTVAL, 3, '0');
 END;
-------------------------------------------------------------
---TRIGGER PARA GENERAR ID DE INTERCAMBIOS
-CREATE OR REPLACE TRIGGER trg_autogenerar_id_intercambios
-BEFORE INSERT ON INTERCAMBIOS
+/
+
+-- TRIGGER PARA GENERAR ID DE INTERCAMBIOS
+CREATE OR REPLACE TRIGGER trg_autogenerar_idIntercambios
+BEFORE INSERT ON Intercambios
 FOR EACH ROW
-DECLARE
-    v_sequence CHAR(3);
-    v_nextval NUMBER;
 BEGIN
-    SELECT seq_in.NEXTVAL INTO v_nextval FROM dual;
-
-    -- Formatear el valor numérico de la secuencia
-    v_sequence := LPAD(v_nextval, 3, '0');
-
-    -- Asignar el valor generado a la columna numero
-    :NEW.numero := 'IN' || v_sequence;
+    :NEW.numero := 'IN' || LPAD(seq_in.NEXTVAL, 3, '0');
 END;
-------------------------------------------------------------
-//TRIGGER PARA GENERAR ID DE COMPRAS
-CREATE OR REPLACE TRIGGER trg_autogenerar_id_compras
-BEFORE INSERT ON COMPRAS
+/
+
+-- TRIGGER PARA GENERAR ID DE COMPRAS
+CREATE OR REPLACE TRIGGER trg_autogenerar_idCompras
+BEFORE INSERT ON Compras
 FOR EACH ROW
-DECLARE
-    v_sequence CHAR(3);
-    v_nextval NUMBER;
 BEGIN
-    SELECT seq_cp.NEXTVAL INTO v_nextval FROM dual;
-
-    -- Formatear el valor numérico de la secuencia
-    v_sequence := LPAD(v_nextval, 3, '0');
-
-    -- Asignar el valor generado a la columna numero
-    :NEW.numero := 'CP' || v_sequence;
+    :NEW.numero := 'CP' || LPAD(seq_cp.NEXTVAL, 3, '0');
 END;
-------------------------------------------------------------
---TRIGGER PARA GENERAR ID DE HISTORIALES
-CREATE OR REPLACE TRIGGER trg_autogenerar_id_historiales
+/
+
+-- TRIGGER PARA GENERAR ID DE HISTORIALES
+CREATE OR REPLACE TRIGGER trg_autogenerar_idHistoriales
 BEFORE INSERT ON HISTORIALES
 FOR EACH ROW
-DECLARE
-    v_sequence CHAR(3);
-    v_nextval NUMBER;
 BEGIN
-    SELECT seq_tr.NEXTVAL INTO v_nextval FROM dual;
-
-    -- Formatear el valor numérico de la secuencia
-    v_sequence := LPAD(v_nextval, 3, '0');
-
-    -- Asignar el valor generado a la columna numero
-    :NEW.numero := 'TR' || v_sequence;
+    :NEW.numero := 'HS' || LPAD(seq_tr.NEXTVAL, 3, '0');
 END;
-------------------------------------------------------------
---TRIGGER PARA GENERAR ID DE MONEDA
+/
+-- TRIGGER PARA GENERAR ID DE MONEDAS
 CREATE OR REPLACE TRIGGER trg_autogenerar_idMoneda
-BEFORE INSERT ON MONEDAS
+BEFORE INSERT ON Monedas
 FOR EACH ROW
 DECLARE
     v_lastFourDigits VARCHAR2(4);
 BEGIN
-    -- Extraer los últimos cuatro dígitos del último idJugador generado
-    SELECT SUBSTR(:NEW.idJugador, 2) INTO v_lastFourDigits FROM dual;
+    -- Extraer los �ltimos cuatro d�gitos del idJugador
+    v_lastFourDigits := SUBSTR(:NEW.jugador, -4);
 
     -- Asignar el valor generado a la columna idMoneda
     :NEW.idMoneda := 'V' || v_lastFourDigits;
 END;
-
-DROP TRIGGER trg_autogenerar_idMoneda;
+/
---
+++ XDisparadores.sql
@@ -0,0 +1,7 @@
+DROP TRIGGER trg_autogenerar_idInventarios;
+DROP TRIGGER trg_autogenerar_idJugador;
+DROP TRIGGER trg_autogenerar_idTransacciones;
+DROP TRIGGER trg_autogenerar_idIntercambios;
+DROP TRIGGER trg_autogenerar_idCompras;
+DROP TRIGGER trg_autogenerar_idHistoriales;
+DROP TRIGGER trg_autogenerar_idMoneda;
